9aae5e499e2187e9021948935b8a1c876e31315f
Patricia Montoya <pmontoya@lean-tech.io>
2019-11-12 16:51:14 -0500

    Adding first configuration files

---
+++ src/API/Routes.js
@@ -0,0 +1,10 @@
+/**
+ * All API routes are separated by method, 
+ * inside every object these routes are 
+ * specified using parameters if needed
+ */
+
+export const GET = {};
+export const POST = {};
+export const UPDATE = {};
+export const DELETE = {};
