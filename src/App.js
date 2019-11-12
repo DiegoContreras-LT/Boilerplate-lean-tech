7cc54a72a83c18f0b3317fb282f88e353fc27257
DcontrerasLeanTech <dcontreras.lean-tech.io>
2019-11-12 16:59:15 -0500

    login with social network

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import {singInWithSocialMedia, singInEmailAndPassword} from './environment/Firebase/Auth';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [state, setState] = useState({
+        email: '',
+        password: ''
+    });
+
+    const handleState = e => {
+        setState({
+            ...state,
+            [e.target.name]: e.target.value
+        })
+    };
+
+    return (
+        <div className="App">
+            <button onClick={() => singInWithSocialMedia('FACEBOOK')}>login con facebook</button>
+            <br/>
+            <br/>
+            <button onClick={() => singInWithSocialMedia('GOOGLE')}>login con Google</button>
+            <br/>
+            <br/>
+            <hr/>
+            <input type="text" name='email' onChange={handleState} value={state.email}/>
+            <br/>
+            <input type="text" name='password' onChange={handleState} value={state.password}/>
+            <br/>
+            <button onClick={() => singInEmailAndPassword(state)}>login</button>
+        </div>
+    );
 }
 
 export default App;
