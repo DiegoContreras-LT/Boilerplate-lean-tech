fb9595b5d7e4591df9060523bbdb388f1a4fcda8
DcontrerasLeanTech <dcontreras.lean-tech.io>
2019-11-14 08:37:24 -0500

    adjusted in config react

--- src/App.js
+++ src/App.js
@@ -1,36 +1,8 @@
-import React, {useState} from 'react';
-import {singInWithSocialMedia, singInEmailAndPassword} from './environment/Firebase/Auth';
+import React from 'react';
+
 import './App.css';
 
 function App() {
-    const [state, setState] = useState({
-        email: '',
-        password: ''
-    });
-
-    const handleState = e => {
-        setState({
-            ...state,
-            [e.target.name]: e.target.value
-        })
-    };
-
-    return (
-        <div className="App">
-            <button onClick={() => singInWithSocialMedia('FACEBOOK')}>login con facebook</button>
-            <br/>
-            <br/>
-            <button onClick={() => singInWithSocialMedia('GOOGLE')}>login con Google</button>
-            <br/>
-            <br/>
-            <hr/>
-            <input type="text" name='email' onChange={handleState} value={state.email}/>
-            <br/>
-            <input type="text" name='password' onChange={handleState} value={state.password}/>
-            <br/>
-            <button onClick={() => singInEmailAndPassword(state)}>login</button>
-        </div>
-    );
 }
 
 export default App;
---
+++ src/redux/reducers/rootReducer.js
@@ -0,0 +1,15 @@
+import {combineReducers} from "redux";
+import storage from 'redux-persist/lib/storage';
+import {persistReducer} from "redux-persist";
+
+const persist = {
+    key: 'app-root',
+    storage,
+    whitelist: []
+};
+
+const rootReducer = combineReducers({
+
+});
+
+export default persistReducer(persist,rootReducer);
