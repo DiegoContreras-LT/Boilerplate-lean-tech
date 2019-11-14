8d9d2f24141ec7c13105da1a7ec76f94a24bb858
DcontrerasLeanTech <dcontreras.lean-tech.io>
2019-11-14 13:36:58 -0500

    themes

---
+++ src/utils/Colors/index.js
@@ -0,0 +1,18 @@
+const DARK_THEME = {
+    primaryColor:'red',
+};
+
+const LIGHT_THEME = {
+    primaryColor:'blue',
+};
+
+const DEFAULT_THEME = {
+    primaryColor:'yellow',
+};
+
+export const THEMES = {
+    DARK_THEME,
+    LIGHT_THEME,
+    DEFAULT_THEME
+};
+
