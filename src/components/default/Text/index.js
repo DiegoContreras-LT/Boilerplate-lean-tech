4702fefacfc5cde797424f5de4410bf0f8507c5f
Cristian Suarez <csuarez@starkcore.com>
2019-11-22 13:54:22 -0500

    feat: Add theme to Text component

--- src/components/default/Text/index.js
+++ src/components/default/Text/index.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import {Paragraph} from './styles'
 import {ArrayString} from './../../../utils/keys'
+import {withChangeTheme} from './../../../HOC/withChangeTheme'
 
-export const Text = (props) => {
+export const TextComponent = (props) => {
 
     ArrayString(props.contentKey, props.content, props.avoid)
-    
+
     return (
        <Paragraph 
-       title={props.title ? props.title : props.Text} 
+       title={props.title ? props.title : props.content} 
        {...props}>
            {props.content}
        </Paragraph>
     )
 }
+
+export const Text = withChangeTheme(TextComponent)
