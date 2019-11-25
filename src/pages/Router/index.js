a547f944db70a32adda868bde2deae31b49fbac7
DiegoContrerasA <dcontreras.lean-tech.io>
2019-11-25 17:06:55 -0500

    adjusted

--- src/pages/Router/index.js
+++ src/pages/Router/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Switch} from "react-router-dom";
 import PrivateRoute from './PrivateRoute';
 import PublicRoute from './PublicRoute';
-import Login from "../auth/login";
+import Login from "../Auth/Login";
 
 const Routes = () => (
     <Switch>
